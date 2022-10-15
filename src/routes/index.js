56438b6ce3a56d2c48ec3bdfd9c8cc73f000da37
Louis Mullie <louis.mullie@gmail.com>
2022-10-15 01:13:49 -0400

    Enable multi-site demo

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', function(req, res, next) {
-  res.status(200).send('CODA-19');
+router.get('/', function (req, res, next) {
+  res.status(200).send('CODA');
 })
 
 module.exports = router
