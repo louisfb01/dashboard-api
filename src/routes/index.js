e646e18b2dbbd5e732667a9a2fa0f54d1c943c32
Louis Mullie <louis.mullie@gmail.com>
2022-12-21 21:53:51 -0500

    Default response

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res, next) {
-  res.status(200).send('CODA');
+  res.status(200).json({ "status": "ok" })
 })
 
 module.exports = router
