0775ba457d48cb9d1397641f6b11c0f279cf4ebc
Louis Mullie <louis.mullie@gmail.com>
2022-12-23 18:57:14 -0500

    Update CorsMiddleware.ts

--- src/middleware/CorsMiddleware.ts
+++ src/middleware/CorsMiddleware.ts
@@ -3,10 +3,11 @@ import cors from 'cors';
 
 function register(app: Application,) {
     const allowedOrigins: Array<string> = [
-        'http://localhost:3000',
-        'https://localhost:3000',
-        ...(process.env.CODA_DASHBOARD_APP_URL ?
-            [process.env.CODA_DASHBOARD_APP_URL] : [])
+        '*',
+        //'http://localhost:3000',
+        //'https://localhost:3000',
+        //...(process.env.CODA_DASHBOARD_APP_URL ?
+        //[process.env.CODA_DASHBOARD_APP_URL] : [])
     ];
 
     const corsOptions = {
